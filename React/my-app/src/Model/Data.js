2feb4ca26500a050eeae26545465f7b06ae3bc6c
nam134778 <62176476+nam134778@users.noreply.github.com>
2021-11-07 15:29:49 +0700

    add data in Data.js

--- React/my-app/src/Model/Data.js
+++ React/my-app/src/Model/Data.js
@@ -5,20 +5,24 @@ import standard from '../Img/Normal/Standard.png'
 
 
 const Cupcake = [
-    {id: 1, image:chicken, title: 'Cupcake', price: 50},
-
+    {id: 1, image:chicken, title: 'Chicken', price: 50},
+    {id: 1, image:chicken, title: 'Chicken2', price: 50},
+    {id: 1, image:chicken, title: 'Chicken3', price: 50},
+    {id: 1, image:chicken, title: 'Chicken4', price: 50},
 ]
 const SeaFood = [
-    {id: 2, image:chicken, title: 'Sea Food', price: 50},
-
+    {id: 2, image:fish, title: 'Fish', price: 35},
+    {id: 2, image:fish, title: 'Fish2', price: 35},
+    {id: 2, image:fish, title: 'Fish3', price: 35},
+    {id: 2, image:fish, title: 'Fish4', price: 35},
 ]
 const Juice = [
-    {id: 3, image:chicken, title: 'Juice', price: 50},
-
+    {id: 3, image:meat, title: 'Meat', price: 24},
+    {id: 3, image:meat, title: 'Meat2', price: 24},
 ]
 const Coca = [
-    {id: 4, image:chicken, title: 'Coca', price: 50},
-
+    {id: 4, image:standard, title: 'Standard Meal', price: 30},
+    {id: 4, image:standard, title: 'Standard Meal2', price: 30},
 ]
 
 const Normal = [
@@ -28,12 +32,12 @@ const Normal = [
     {id: 1, image:standard, title: 'Standard Meal', price: 30},
     {id: 1, image:chicken, title: 'Chicken2', price: 50},
     {id: 1, image:fish, title: 'Fish2', price: 35},
-    {id: 1,image:chicken, title: 'Chicken', price: 50},
-    {id: 1, image:fish, title: 'Fish', price: 35},
-    {id: 1, image:meat, title: 'Meat', price: 24},
-    {id: 1, image:standard, title: 'Standard Meal', price: 30},
-    {id: 1, image:chicken, title: 'Chicken2', price: 50},
-    {id: 1, image:fish, title: 'Fish2', price: 35},
+    {id: 1,image:chicken, title: 'Chicken3', price: 50},
+    {id: 1, image:fish, title: 'Fish3', price: 35},
+    {id: 1, image:meat, title: 'Meat2', price: 24},
+    {id: 1, image:standard, title: 'Standard Meal2', price: 30},
+    {id: 1, image:chicken, title: 'Chicken4', price: 50},
+    {id: 1, image:fish, title: 'Fish4', price: 35},
 ]
 
 export { Normal, Coca, Cupcake, SeaFood, Juice }
