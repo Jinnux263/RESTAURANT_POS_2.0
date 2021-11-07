d54d3898a1e0363065225a6ed6bc2819b158f3b6
nam134778 <62176476+nam134778@users.noreply.github.com>
2021-11-07 22:58:42 +0700

    Update new dishes for data.js

--- React/my-app/src/Model/Data.js
+++ React/my-app/src/Model/Data.js
@@ -1,44 +1,71 @@
-import chicken from "../Img/Normal/Chicken.png"
-import fish from "../Img/Normal/Fish.png"
-import meat from "../Img/Normal/Meat.png"
-import standard from '../Img/Normal/Standard.png'
+import chicken from "../Img/Normal/Chicken.jpg"
+import chocolate from "../Img/Normal/Chocolate.jpg"
+import coca from "../Img/Normal/coca.jpg"
+import icecream from "../Img/Normal/Icecream.jpg"
+import juice from "../Img/Normal/juice.jpg"
+import kiwi from "../Img/Normal/kiwi.jpg"
+import lamp from "../Img/Normal/lamp.jpg"
+import parfait from "../Img/Normal/Parfait.png"
+import risotto from "../Img/Normal/Risotto.jpg"
+import salmon from "../Img/Normal/Salmon.jpg"
+import sashimi from "../Img/Normal/Sashimi.png"
+import spaghetti from "../Img/Normal/Spaghetti.jpg"
+import squid from "../Img/Normal/Squid.jpg"
+import steak from "../Img/Normal/Steak.jpg"
 
-
-const Cupcake = [
-    {id: 1, image:chicken, title: 'Chicken', price: 50},
-    {id: 1, image:chicken, title: 'Chicken2', price: 50},
-    {id: 1, image:chicken, title: 'Chicken3', price: 50},
-    {id: 1, image:chicken, title: 'Chicken4', price: 50},
+const Meat = [
+    {id: 1, image:chicken, title: 'Chicken', price: 75},
+    {id: 1, image:lamp, title: 'Lamp', price: 40},
+    {id: 1, image:steak, title: 'Beefsteak', price: 50},
+    {id: 1, image:spaghetti, title: 'Spaghetti', price: 25},
 ]
 const SeaFood = [
-    {id: 2, image:fish, title: 'Fish', price: 35},
-    {id: 2, image:fish, title: 'Fish2', price: 35},
-    {id: 2, image:fish, title: 'Fish3', price: 35},
-    {id: 2, image:fish, title: 'Fish4', price: 35},
+    {id: 2, image:risotto, title: 'Risotto', price: 45},
+    {id: 2, image:salmon, title: 'Salmon', price: 35},
+    {id: 2, image:sashimi, title: 'Sashimi', price: 55},
+    {id: 2, image:squid, title: 'Squid Fried', price: 30},
 ]
-const Juice = [
-    {id: 3, image:meat, title: 'Meat', price: 24},
-    {id: 3, image:meat, title: 'Meat2', price: 24},
+const Dessert = [
+    {id: 3, image:chocolate, title: 'Chocolate Pie', price: 20},
+    {id: 3, image:icecream, title: 'Ice Cream', price: 24},
+    {id: 3, image:parfait, title: 'Parfait', price: 30},
+    {id: 3, image:kiwi, title: 'Kiwi Pie', price: 32},
 ]
-const Coca = [
-    {id: 4, image:standard, title: 'Standard Meal', price: 30},
-    {id: 4, image:standard, title: 'Standard Meal2', price: 30},
-]
-
-const Normal = [
-    {id: 1,image:chicken, title: 'Chicken', price: 50},
-    {id: 1, image:fish, title: 'Fish', price: 35},
-    {id: 1, image:meat, title: 'Meat', price: 24},
-    {id: 1, image:standard, title: 'Standard Meal', price: 30},
-    {id: 1, image:chicken, title: 'Chicken2', price: 50},
-    {id: 1, image:fish, title: 'Fish2', price: 35},
-    {id: 1,image:chicken, title: 'Chicken3', price: 50},
-    {id: 1, image:fish, title: 'Fish3', price: 35},
-    {id: 1, image:meat, title: 'Meat2', price: 24},
-    {id: 1, image:standard, title: 'Standard Meal2', price: 30},
-    {id: 1, image:chicken, title: 'Chicken4', price: 50},
-    {id: 1, image:fish, title: 'Fish4', price: 35},
-    
+const Drink = [
+    {id: 4, image:coca, title: 'Coca', price: 5},
+    {id: 4, image:juice, title: 'Juice', price: 15},
 ]
 
-export { Normal, Coca, Cupcake, SeaFood, Juice }
+const All = [
+    {id: 1, image:chicken, title: 'Chicken', price: 75},
+    {id: 1, image:lamp, title: 'Lamp', price: 40},
+    {id: 1, image:steak, title: 'Beefsteak', price: 50},
+    {id: 1, image:spaghetti, title: 'Spaghetti', price: 25},
+    {id: 1, image:risotto, title: 'Risotto', price: 45},
+    {id: 1, image:salmon, title: 'Salmon', price: 35},
+    {id: 1, image:sashimi, title: 'Sashimi', price: 55},
+    {id: 1, image:squid, title: 'Squid Fried', price: 30},
+    {id: 1, image:chocolate, title: 'Chocolate Pie', price: 20},
+    {id: 1, image:icecream, title: 'Ice Cream', price: 24},
+    {id: 1, image:parfait, title: 'Parfait', price: 30},
+    {id: 1, image:kiwi, title: 'Kiwi Pie', price: 32},
+    {id: 1, image:coca, title: 'Coca', price: 5},
+    {id: 1, image:juice, title: 'Juice', price: 15},
+
+    {image:chicken, title: 'Chicken', text: '$75.000'},
+    {image:lamp, title: 'Lamp', text: '$40.000'},
+    {image:steak, title: 'Beefsteak', text: '$50.000'},
+    {image:spaghetti, title: 'Spaghetti', text: '$25.000'},
+    {image:risotto, title: 'Risotto', text: '$45.000'},
+    {image:salmon, title: 'Salmon', text: '$35.000'},
+    {image:sashimi, title: 'Sashimi', text: '$55.000'},
+    {image:squid, title: 'Squid Fried', text: '$30.000'},
+    {image:chocolate, title: 'Chocolate Pie', text: '$20.000'},
+    {image:icecream, title: 'Ice Cream', text: '$24.000'},
+    {image:parfait, title: 'Parfait', text: '$30.000'},
+    {image:kiwi, title: 'Kiwi Pie', text: '$32.000'},
+    {image:coca, title: 'Coca', text: '$5.000'},
+    {image:juice, title: 'Juice', text: '$15.000'},
+]
+
+export { All , Meat, SeaFood, Dessert, Drink}
