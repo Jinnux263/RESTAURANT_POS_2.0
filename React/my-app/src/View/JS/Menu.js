c71854f8870488b5de3e1f1eb72a16ec3c768f28
nam134778 <62176476+nam134778@users.noreply.github.com>
2021-11-07 22:59:58 +0700

    Update dishes so update menu, too!

--- React/my-app/src/View/JS/Menu.js
+++ React/my-app/src/View/JS/Menu.js
@@ -3,11 +3,12 @@ import { useState } from 'react';
 import MenuList from './MenuList/MenuList';
 import MealInfo from './MenuInfo/MealInfo';
 import '../CSS/Menu.css'
-import { Normal , Coca, Cupcake, SeaFood, Juice} from '../../Model/Data';
+import { All , Meat, SeaFood, Dessert, Drink} from '../../Model/Data';
+// import { BsFillHouseDoorFill } from "react-icons/bs";
 
 
 function Menu() {
-    const [ShowList, setShowList] = useState(Normal);
+    const [ShowList, setShowList] = useState(All);
     const [cartItems, setCartItems] = useState([]);
 
     const handleAddToCart = (clickedItem) => {
@@ -26,38 +27,42 @@ function Menu() {
             return [...prev, { ...clickedItem, amount: 1 }];
             });
     }
-
-    const HandleClearCart = () => {
-        setCartItems([]);
-    }
+    //console.log(cartItems)
+    // const handleAddToCart = (clickedItem) => {
+    //     //console.log(clickedItem)
+    //     setCartItems(prev => {
+    //         return [...prev, clickedItem];
+    //     });
+    // }
 
     const handleClickCata = (item) => {
         setShowList(item)
     }
-    console.log(cartItems)
+
     return (
         <Container fluid>
             <Row>
                 <Col xs = {9} className="Menucontainer" >
                     <Row>
                         <CloseButton className="GoBackBtn"/>
+                        {/* <BsFillHouseDoorFill/> */}
                     </Row>
                     <Row>
                         <CardGroup>
-                            <Card.Body className = 'CataBlock' onClick = {() => handleClickCata(Normal)}>
-                                <Card.Title>Normal</Card.Title>
+                            <Card.Body className = 'CataBlock' onClick = {() => handleClickCata(All)}>
+                                <Card.Title>All</Card.Title>
                             </Card.Body>
-                            <Card.Body className = 'CataBlock' onClick = {() => handleClickCata(Cupcake)}>
-                                <Card.Title>Cupcake</Card.Title>
+                            <Card.Body className = 'CataBlock' onClick = {() => handleClickCata(Meat)}>
+                                <Card.Title>Meat</Card.Title>
                             </Card.Body>
                             <Card.Body className = 'CataBlock' onClick = {() => handleClickCata(SeaFood)}>
                                 <Card.Title>Sea Food</Card.Title>
                             </Card.Body>
-                            <Card.Body className = 'CataBlock' onClick = {() => handleClickCata(Juice)}>
-                                <Card.Title>Juice</Card.Title>
+                            <Card.Body className = 'CataBlock' onClick = {() => handleClickCata(Dessert)}>
+                                <Card.Title>Dessert</Card.Title>
                             </Card.Body>
-                            <Card.Body className = 'CataBlock' onClick = {() => handleClickCata(Coca)}>
-                                <Card.Title>Coca</Card.Title>
+                            <Card.Body className = 'CataBlock' onClick = {() => handleClickCata(Drink)}>
+                                <Card.Title>Drink</Card.Title>
                             </Card.Body>
                         </CardGroup>
                     </Row>
@@ -66,7 +71,7 @@ function Menu() {
                     </Row>
                 </Col>
                 <Col> 
-                    <MealInfo CartList = {cartItems} handlefunc = {HandleClearCart}/>
+                    <MealInfo CartList = {cartItems}/>
                 </Col>
             </Row>
     </Container>
