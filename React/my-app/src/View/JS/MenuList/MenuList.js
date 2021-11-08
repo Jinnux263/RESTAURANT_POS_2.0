f9deb4f5cc43c3bf2bd8ab1208d187156d6a8ce6
Jinnux <nguyentandat26032001@gmail.com>
2021-11-09 00:01:52 +0700

    Create Figure 3

--- React/my-app/src/View/JS/MenuList/MenuList.js
+++ React/my-app/src/View/JS/MenuList/MenuList.js
@@ -1,13 +1,24 @@
-import { Card, Button, Row, Col } from 'react-bootstrap';
+import { Card, Button, Row, Col, Modal } from 'react-bootstrap';
+import { useState } from 'react';
 import '../../CSS/Menu.css'
 
 function MenuList(props) {
     //console.log(props.handlefunc)
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
+        <>
+
+        {
+            //The Meal list
+        }
         <div className="grid">
             {props.Mlist.map( (MealInfo , index) => {
                 return (
-                    <Card style={{ width: '18rem' }} key={index} className="box">
+                    <Card style={{ width: '18rem' }} key={index} className="box" onClick={handleShow}>
                         <Card.Img variant="top" src={MealInfo.image} className = "card-img-top" />
                         <Card.Body>
                             <Card.Title>
@@ -20,7 +31,7 @@ function MenuList(props) {
                                     </Card.Text>
                                 </Col>
                                 <Col>
-                                    <Button variant="primary" className="btn" onClick = {() => props.handlefunc(MealInfo)}>Add</Button>
+                                    <Button variant="primary" className="btn" onClick = {(e) => {e.stopPropagation(); props.handlefunc(MealInfo)}}>Add</Button>
                                 </Col>
                             </Row>
                         </Card.Body>
@@ -29,6 +40,25 @@ function MenuList(props) {
                 }    
             )}
         </div>
+
+        {
+            //Popup when click to meal
+        }
+        <Modal show={show} onHide={handleClose} centered backdrop="static">
+            <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                </Button>
+            </Modal.Footer>
+        </Modal>
+        </>
     )
 }
 
