01dd22ecd7fa6031bea504d46dd033c41d6fe827
nam134778 <62176476+nam134778@users.noreply.github.com>
2021-11-10 22:35:31 +0700

    Update Item.js

--- React/my-app/src/View/JS/MenuInfo/Item.js
+++ React/my-app/src/View/JS/MenuInfo/Item.js
@@ -3,20 +3,22 @@ import {Col, Row, Button} from 'react-bootstrap';
 function Item(props) {
     //console.log(props.input)
     return (
-        <Row className="cartItem">
+        <Row className="cartItem" style={{paddingRight:"1.4rem"}}>
             
-            <Col>
+            <Row>
                 {props.input.title}:
-            </Col>
-            <Col xs = {2}>
+            </Row>
+            <Row>
+            <Col xs = {4} md={1} sm={4}>
                 <Button onClick = {() => props.decFunc(props.input)}>-</Button>
             </Col>
-            <Col xs = {1}>
+            <Col xs = {4} md ={1} style={{paddingLeft:"2rem"}}>
                 {props.input.amount}
             </Col>
-            <Col xs = {2}>
+            <Col xs = {4} md={2} sm={1}>
                 <Button onClick = {() => props.incFunc(props.input)}>+</Button>
             </Col>
+            </Row>
         </Row>
     )
 }
